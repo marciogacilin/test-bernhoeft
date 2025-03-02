f51de7519a96a0e3b420fc1b8391b32627785bf3
Marcio Gacilin <marciogacilin@hotmail.com>
2025-03-02 07:33:38 -0300

    Ajuste

--- 2-Application/Bernhoeft.GRT.Teste.Application/Responses/Queries/v1/GetAvisosResponse.cs
+++ 2-Application/Bernhoeft.GRT.Teste.Application/Responses/Queries/v1/GetAvisosResponse.cs
@@ -7,12 +7,16 @@ namespace Bernhoeft.GRT.Teste.Application.Responses.Queries.v1
         public int Id { get; set; }
         public bool Ativo { get; set; }
         public string Titulo { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public DateTime? EditadoEm { get; set; }
 
         public static implicit operator GetAvisosResponse(AvisoEntity entity) => new()
         {
             Id = entity.Id,
             Ativo = entity.Ativo,
-            Titulo = entity.Titulo
+            Titulo = entity.Titulo,
+            CriadoEm = entity.CriadoEm,
+            EditadoEm = entity.EditadoEm,
         };
     }
 }
